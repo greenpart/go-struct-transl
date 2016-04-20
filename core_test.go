a30f8298faa1c585ecd14979aefcab32c0444f83
Sergei Zimakov <zimakov@gmail.com>
2016-04-21 01:24:46 +0300

    Add test for initial version

--- core_test.go
+++ core_test.go
@@ -1,6 +1,7 @@
 package godatai18n
 
 import (
+	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
 	"golang.org/x/text/language"
 	"testing"
@@ -74,3 +75,86 @@ func realTranslator(in TrType) TrType {
 func BenchmarkNoopTranslator(b *testing.B)      { benchmarkTranslator(in, noopTranslator, b) }
 func BenchmarkFixedLangTranslator(b *testing.B) { benchmarkTranslator(in, fixedLangTranslator, b) }
 func BenchmarkRealTranslator(b *testing.B)      { benchmarkTranslator(in, realTranslator, b) }
+
+func genTrObj() TrType {
+	return TrType{
+		Name:    "",
+		Element: "",
+		Translations: Translations{
+			"en": map[string]string{
+				"name":    "John",
+				"element": "water",
+			},
+			"ru": map[string]string{
+				"name":    "Джон",
+				"element": "вода",
+			},
+		},
+	}
+}
+
+func TestPerfectCase(t *testing.T) {
+	enCtx := NewContextWithAcceptedLanguages(context.Background(), []language.Tag{language.Make("en")})
+
+	o := genTrObj()
+	TranslateOne(enCtx, &o, TrTypeTrProvider{})
+
+	assert.Equal(t, "John", o.Name)
+	assert.Equal(t, "water", o.Element)
+}
+
+func TestPerfectCaseWithSecondLang(t *testing.T) {
+	ruEnCtx := NewContextWithAcceptedLanguages(context.Background(), []language.Tag{language.Make("ru"), language.Make("en")})
+
+	o := genTrObj()
+	TranslateOne(ruEnCtx, &o, TrTypeTrProvider{})
+
+	assert.Equal(t, "Джон", o.Name)
+	assert.Equal(t, "вода", o.Element)
+}
+
+func TestMissingFirstLang(t *testing.T) {
+	jaEnCtx := NewContextWithAcceptedLanguages(context.Background(), []language.Tag{language.Make("ja"), language.Make("en")})
+
+	o := genTrObj()
+	TranslateOne(jaEnCtx, &o, TrTypeTrProvider{})
+
+	assert.Equal(t, "John", o.Name)
+	assert.Equal(t, "water", o.Element)
+}
+
+func TestMissingAllLangsUseEn(t *testing.T) {
+	jaPtCtx := NewContextWithAcceptedLanguages(context.Background(), []language.Tag{language.Make("ja"), language.Make("pt")})
+
+	o := genTrObj()
+	TranslateOne(jaPtCtx, &o, TrTypeTrProvider{})
+
+	assert.Equal(t, "John", o.Name)
+	assert.Equal(t, "water", o.Element)
+}
+
+func TestNoLangInContextUseEn(t *testing.T) {
+	o := genTrObj()
+	TranslateOne(context.Background(), &o, TrTypeTrProvider{})
+
+	assert.Equal(t, "John", o.Name)
+	assert.Equal(t, "water", o.Element)
+}
+
+func TestNoEnValuesForDefaltsUsesRu(t *testing.T) {
+	o := genTrObj()
+	o.Translations["en"] = map[string]string{}
+	TranslateOne(context.Background(), &o, TrTypeTrProvider{})
+
+	assert.Equal(t, "Джон", o.Name)
+	assert.Equal(t, "вода", o.Element)
+}
+
+func TestNoValues(t *testing.T) {
+	o := genTrObj()
+	o.Translations = Translations{}
+	TranslateOne(context.Background(), &o, TrTypeTrProvider{})
+
+	assert.Equal(t, "", o.Name)
+	assert.Equal(t, "", o.Element)
+}
